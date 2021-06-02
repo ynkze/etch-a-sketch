cbd2c17bf1dad339f5c6443fa35c3e2955c8c9fa
ynkze <teyyiheng@gmail.com>
2021-06-02 05:27:02 -0700

    Final changes

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # etch-a-sketch
 Pixel drawing project built using Javascript DOM methods and CSS
-Available at ynkze.github.io/etch-a-sketch/
+
+Available at https://ynkze.github.io/etch-a-sketch/
+
+## Learning Point
+* DOM maniuplation to create multiple divs/grids dynamically
+* DOM nodes to be converted to array before manipulation
+* Using functions to repeat similar tasks e.g. changing colour
--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 //create grid inside div named container
 const container = document.querySelector(".grid-container");
 function createGrid(size) {
@@ -10,38 +9,71 @@ function createGrid(size) {
         container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     }
 }
+
 createGrid(16);
 
-//set up grid and hover effect
-function changeColor(e){
-    this.style.backgroundColor = "black";
-}
-
-function hoverEffect() {
+//set up color and hover effect
+function hoverEffect(color) {
     let grids = document.querySelectorAll(".grid-item");
     let gridsArr =  Array.from(grids);
     gridsArr.forEach(element =>
-        element.addEventListener("mouseover", changeColor)
-    );
+        element.addEventListener("mouseover", ()=>{
+            element.style.backgroundColor = color;
+        }));
 }
-hoverEffect();
+
+hoverEffect("black");
 
 //buttons declaration
 const resetBtn = document.querySelector("#reset");
 const newBtn = document.querySelector("#new");
+const randomBtn = document.querySelector("#random");
+const colorBtn = document.querySelector("#changeColor");
+const eraseBtn = document.querySelector("#erase");
 
+//reset
 resetBtn.addEventListener("click", ()=>{
     container.querySelectorAll('.grid-item').forEach(element =>
         element.style.backgroundColor = "white");
+    hoverEffect("black");
 });
 
+//new grid;
 newBtn.addEventListener("click", ()=>{
     let gridSize=prompt("Input the number of grids you want (max 64)");
     if (gridSize>64){
         alert("Too big! Try a smaller size.");
-        return;
+        gridSize=16;
     }
-    container.querySelectorAll('.grid-item').forEach(e => e.remove());
+    if (isNaN(gridSize)|| gridSize.length===0){
+        gridSize=16;
+        alert("Invalid number")
+    }
+    container.querySelectorAll('.grid-item').forEach(e => e.remove()); // remove all grids,create grid again, initialize hover effect
     createGrid(gridSize);
-    hoverEffect();
+    hoverEffect("black");
+});
+
+//random
+randomBtn.addEventListener("click", ()=>{
+    let grids = document.querySelectorAll(".grid-item");
+    let gridsArr =  Array.from(grids);
+    gridsArr.forEach(element =>
+        element.addEventListener("mouseover", ()=>{
+            let randomColor = Math.floor(Math.random()*16777215).toString(16);
+            element.style.backgroundColor = "#"+randomColor;
+        }))
+});
+
+//change color
+colorBtn.addEventListener("click", ()=>{
+    let colorChoice = prompt("Input color you want (hex code or name)");
+    colorChoice.toString().toLowerCase();
+    hoverEffect(colorChoice);
+});
+
+
+//erase
+eraseBtn.addEventListener("click", ()=>{
+    hoverEffect("white");
 });
